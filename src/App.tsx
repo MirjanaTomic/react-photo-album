2897987e2b16729393c7a7d7e211633506b64cbc
MirjanaTomic <mirjanatomic@yahoo.com>
2024-03-01 16:46:18 +0100

    task/01-listuser: Created User interface, created Table component and populated it with data fetched from JSONPlaceholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+function Table() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(`${USERS_URL}`);
+      const users = (await response.json()) as User[];
+      setUsers(users);
+    };
+
+    fetchUsers();
+  }, []);
+  return (
+    <div className="Table">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Address</th>
+        </tr>
+        {users.map((val, key) => {
+          console.log(val, key, "val key");
+          return (
+            <tr className="App-table-data-row" key={key}>
+              <td>{val.name}</td>
+              <td>
+                {val.address?.street}, {val.address?.zipcode},
+                {val.address?.city}
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Photo Album App</h1>
       </header>
+      <div className="Table-section">
+        <Table />
+      </div>
     </div>
   );
 }
