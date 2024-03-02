2e1c202901a48098c1870099bb22d2112e73db83
MirjanaTomic <mirjanatomic@yahoo.com>
2024-03-02 13:23:18 +0100

    task/03-viewalbums: Added React Router, created 'not found' view and added base route

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: (
+      <div className="App-not-found">
+        <span>404 Page Not Found</span>
+      </div>
+    ),
+  },
+  {
+    path: "/userAlbums",
+    element: <div>Test</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
